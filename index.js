8a8ece05207bd274728788ee5c5fe5c578e4d18f
ludvigla <ludvigla@kth.se>
2022-11-30 14:29:18 +0100

    Solution to 3.19*: Phonebook database, step7

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (body.name === undefined | body.number === undefined) {
@@ -41,6 +41,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -71,14 +72,13 @@ const unknownEndpoint = (request, response) => {
 
 // Update person
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+      request.params.id, 
+      { name, number }, 
+      { new: true, runValidators: true, context: 'query' }
+    )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -93,6 +93,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
